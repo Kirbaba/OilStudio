416a0c7a37f6da9f978653d96abc335c9ad65563
Vladislav <v.kirbaba@gmail.com>
2016-02-20 11:45:25 +0300

    Desktop

--- js/script.js
+++ js/script.js
@@ -1,37 +1,4 @@
-
-/*-------------GOOGLE MAPS-----------------*/
-
-/*function initialize() {
-
-    var myLatlng = new google.maps.LatLng(59.934602, 30.334607);
-    var mapOptions = {
-        center: new google.maps.LatLng(59.934602, 30.334607),
-        zoom: 17,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        scrollwheel: false
-    };
-    var map = new google.maps.Map(document.getElementById("map_canvas"),
-        mapOptions);
-    var marker = new google.maps.Marker({
-        position: myLatlng,
-        map: map,
-        title:"Ditlogistic"
-    });
-}
-
-function loadScript() {
-    var script = document.createElement("script");
-    script.type = "text/javascript";
-    script.src = "http://maps.googleapis.com/maps/api/js?key=AIzaSyAaOWKyamSxMTXclSDFmJ2N4Am20PCTD6I&sensor=FALSE&callback=initialize";
-    document.body.appendChild(script);
-}
-
-window.onload = loadScript;
-*/
-
-
 $(function() {
-
     $(window).scroll(function() {
         if($(this).scrollTop() != 0) {
             $('#toTop').fadeIn();
@@ -131,3 +98,7 @@ $(function(){
     });
 });
 
+$( document ).ready(function() {
+    var insta_h = $('.reviews__item--img').width();
+    $(".reviews__item--img").height(insta_h);
+});
